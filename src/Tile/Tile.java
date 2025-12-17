ab71fff5577a334aa855fce1ae84e1718baa804b
klgfernandez01202401052-lang <klgfernandez01202401052@usep.edu.ph>
2025-12-18 00:53:25 +0800

    Update Tile.java

--- src/Tile/Tile.java
+++ src/Tile/Tile.java
@@ -5,12 +5,17 @@ import java.awt.image.BufferedImage;
 
 public class Tile {
 
-    public BufferedImage image;
-    public boolean collision = false;
+    public BufferedImage image;                
+    // Tile sprite
+    public boolean collision = false;         
+    // True if this tile blocks movement
 
-    public Rectangle solidArea = new Rectangle(0, 0, 16, 16);
-    public int solidAreaDefaultX = 0;
-    public int solidAreaDefaultY = 0;
+    public Rectangle solidArea = new Rectangle(0, 0, 16, 16); // Precise collision box
+    public int solidAreaDefaultX = 0;        
+    // Default X offset for resetting
+    public int solidAreaDefaultY = 0;        
+    // Default Y offset for resetting
 
-    public boolean front = false;
+    public boolean front = false;           
+    // If true, drawn in front of entities
 }
